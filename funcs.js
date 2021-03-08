011dbaf57d582b268ba5c3947934f38f52021328
KallerMG <50585470+KallerMG@users.noreply.github.com>
2021-03-08 16:55:37 -0300

    JS pt3(Array)

--- funcs.js
+++ funcs.js
@@ -43,7 +43,33 @@ let endereco_var = "endereco";
 let numero_da_rua_var = "numero";
 console.log(user[endereco_var][numero_da_rua_var]);
 
+const students = [
+    { id: 1, name: 'John Doe', age: 23, scholarship: false },
+    { id: 2, name: 'Margoe Rose', age: 19, scholarship: true },
+    { id: 3, name: 'Kayle', age: 22, scholarship: true }
+]
 
+function somente_nomes(item) {
+    console.log(item["name"]);
+};
 
+students.forEach(somente_nomes);
 
+function maior_d_20(item) {
+    if (item["age"] > 20) {
+        console.log(item);
+    };
+};
 
+students.forEach(maior_d_20);
+
+let novo_arr = [];
+
+function tem_bolsa(item) {
+    if (item["scholarship"] == true) {
+        novo_arr.push(item);
+    };
+};
+
+students.forEach(tem_bolsa)
+console.log(novo_arr);
