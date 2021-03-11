5e8bd656ebbdf6186ad5136b4ed598c24faf1bc9
KallerMG <50585470+KallerMG@users.noreply.github.com>
2021-03-11 11:10:57 -0300

    Desafio JS pt.4 extra

--- funcs.js
+++ funcs.js
@@ -1,121 +1,69 @@
-const colaboradores = [
+const installments = [
     {
-        name: 'Juca',
-        idade: 25,
-        cargo: 'front-end'
+        installment_number: 1,
+        value: 123.45,
+        status: 'Pago'
     },
     {
-        name: 'Márcia',
-        idade: 23,
-        cargo: 'back-end'
+        installment_number: 2,
+        value: 139.88,
+        status: 'Pago'
     },
     {
-        name: 'Vitória',
-        idade: 28,
-        cargo: 'designer'
+        installment_number: 3,
+        value: 123.45,
+        status: 'Pago'
     },
     {
-        name: 'Fernando',
-        idade: 19,
-        cargo: 'estagiario'
+        installment_number: 4,
+        value: 182.37,
+        status: 'Aberto'
     },
     {
-        name: 'Fabiane',
-        idade: 25,
-        cargo: 'back-end'
-    },
-    {
-        name: 'Jéssica',
-        idade: 23,
-        cargo: 'front-end'
-    },
-]
-
-const resultado_1 = colaboradores.map(colab => {
-    return colab.name;
-});
-
-console.log(resultado_1);
-
-const resultado_2 = colaboradores.filter(colab => {
-    return colab.cargo == 'front-end';
-});
-
-console.log(resultado_2);
-
-const resultado_3 = colaboradores.find(colab => {
-    return colab.idade > 23;
-});
-
-console.log(resultado_3);
-
-const resultado_4 = colaboradores.every(colab => {
-    return colab.idade > 18;
-});
-
-console.log("São Maiores de 18?", resultado_4);
-
-const resultado_5 = colaboradores.some(colab => {
-    return colab.cargo == 'estagiario';
-});
-
-console.log("Existe algum estagiário?", resultado_5);
-
-
-const resultadoD2_1_a = colaboradores.reduce((acumulador, colab) => {
-    return acumulador + colab.idade;
-}, 0);
-
-console.log("Soma das idades:", resultadoD2_1_a);
-
-const resultadoD2_1_b = colaboradores.reduce((acc, colab) => Object.assign(acc, { [colab.cargo]: acc[colab.cargo] ? acc[colab.cargo] + 1 : 1 }), {});
-
-console.log("Resultado D2 1 b:", resultadoD2_1_b);
-
-// Colaboradores crescente
-///const resultadoD2_2_a = colaboradores.sort((a, b) => {
-// return a.idade - b.idade;
-//});
-
-//console.log("Resultado D2 2 a:", colaboradores);
-
-// Colaboradores decrescente
-///const resultadoD2_2_b = colaboradores.sort((a, b) => {
-// return b.idade - a.idade;
-//});
-
-//console.log("Resultado D2 2 b:", colaboradores);
-
-
-/* const resultadoD2_2_c = colaboradores.sort((a, b) => {
-    let pesos = {
-        'back-end': 3,
-        'front-end': 2,
-        'designer': 4,
-        'estagiario': 1
-    };
-    return pesos[a.cargo] - pesos[b.cargo];
-});
-
-console.log("Resultado D2 2 c:", resultadoD2_2_c); */
-
-const resultadoD2_2_d = colaboradores.sort((a, b) => {
-    let pesos = {
-        'back-end': 3,
-        'front-end': 2,
-        'designer': 4,
-        'estagiario': 1
-    };
-    if (a.idade > b.idade) {
-        return 1;
+        installment_number: 5,
+        value: 133.93,
+        status: 'Aberto'
     }
-    if (a.idade < b.idade) {
-        return -1;
+];
+
+/* const resultado_DE_a = installments.reduce((acumulador, insta) => {
+    return acumulador + insta.value;
+}, 0)
+
+console.log("Valor total das parcelas:", resultado_DE_a.toFixed(2)); */
+
+/* const resultado_DE_b = installments.reduce((acc, insta) => {
+    if (insta.status == 'Pago') {
+        return {
+            total_pago: acc.total_pago + 1,
+            total_aberto: acc.total_aberto
+        };
+    } else {
+        return {
+            total_pago: acc.total_pago,
+            total_aberto: acc.total_aberto + 1
+        };
     }
-    if (a.idade == b.idade) {
-        return pesos[a.cargo] - pesos[b.cargo];
-    }
-    return 0;
+
+}, {
+    total_pago: 0,
+    total_aberto: 0
+})
+
+console.log("Valor total das parcelas:", resultado_DE_b); */
+
+
+/* const resultado_DE_c = installments.sort((a, b) => {
+    return b.value - a.value;
 });
 
-console.log("Resultado D2 2 c:", resultadoD2_2_d);
+console.log("Forma desc", resultado_DE_c);
+ */
+const resultado_DE_d = installments.sort((a, b) => {
+    return ((a.value - b.value) == 0) ? (b.installment_number - a.installment_number) : a.value - b.value
+});
+
+console.log("Forma crescente:", resultado_DE_d);
+
+
+
---
+++ funcs_4.js
@@ -0,0 +1,125 @@
+const colaboradores = [
+    {
+        name: 'Juca',
+        idade: 25,
+        cargo: 'front-end'
+    },
+    {
+        name: 'Márcia',
+        idade: 23,
+        cargo: 'back-end'
+    },
+    {
+        name: 'Vitória',
+        idade: 28,
+        cargo: 'designer'
+    },
+    {
+        name: 'Fernando',
+        idade: 19,
+        cargo: 'estagiario'
+    },
+    {
+        name: 'Fabiane',
+        idade: 25,
+        cargo: 'back-end'
+    },
+    {
+        name: 'Jéssica',
+        idade: 23,
+        cargo: 'front-end'
+    },
+]
+
+const resultado_1 = colaboradores.map(colab => {
+    return colab.name;
+});
+
+console.log(resultado_1);
+
+const resultado_2 = colaboradores.filter(colab => {
+    return colab.cargo == 'front-end';
+});
+
+console.log(resultado_2);
+
+const resultado_3 = colaboradores.find(colab => {
+    return colab.idade > 23;
+});
+
+console.log(resultado_3);
+
+const resultado_4 = colaboradores.every(colab => {
+    return colab.idade > 18;
+});
+
+console.log("São Maiores de 18?", resultado_4);
+
+const resultado_5 = colaboradores.some(colab => {
+    return colab.cargo == 'estagiario';
+});
+
+console.log("Existe algum estagiário?", resultado_5);
+
+
+const resultadoD2_1_a = colaboradores.reduce((acumulador, colab) => {
+    return acumulador + colab.idade;
+}, 0);
+
+console.log("Soma das idades:", resultadoD2_1_a);
+
+const resultadoD2_1_b = colaboradores.reduce((acc, colab) => Object.assign(acc, { [colab.cargo]: acc[colab.cargo] ? acc[colab.cargo] + 1 : 1 }), {});
+
+console.log("Resultado D2 1 b:", resultadoD2_1_b);
+
+// Colaboradores crescente
+///const resultadoD2_2_a = colaboradores.sort((a, b) => {
+// return a.idade - b.idade;
+//});
+
+//console.log("Resultado D2 2 a:", colaboradores);
+
+// Colaboradores decrescente
+///const resultadoD2_2_b = colaboradores.sort((a, b) => {
+// return b.idade - a.idade;
+//});
+
+//console.log("Resultado D2 2 b:", colaboradores);
+
+
+/* const resultadoD2_2_c = colaboradores.sort((a, b) => {
+    let pesos = {
+        'back-end': 3,
+        'front-end': 2,
+        'designer': 4,
+        'estagiario': 1
+    };
+    return pesos[a.cargo] - pesos[b.cargo];
+});
+
+console.log("Resultado D2 2 c:", resultadoD2_2_c); */
+
+const resultadoD2_2_d = colaboradores.sort((a, b) => {
+    let pesos = {
+        'back-end': 3,
+        'front-end': 2,
+        'designer': 4,
+        'estagiario': 1
+    };
+    if (a.idade > b.idade) {
+        return 1;
+    }
+    if (a.idade < b.idade) {
+        return -1;
+    }
+    if (a.idade == b.idade) {
+        return pesos[a.cargo] - pesos[b.cargo];
+    }
+    return 0;
+});
+
+console.log("Resultado D2 2 c:", resultadoD2_2_d);
+
+
+
+
